f93b57ac0e3b85e2cb076b35e2f98be4d29c1d29
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-11 03:07:50 +0400

    MenuadminUpdateIsComplated

--- FinalProjectRestorant/Areas/AdminPanel/Controllers/MenuController.cs
+++ FinalProjectRestorant/Areas/AdminPanel/Controllers/MenuController.cs
@@ -1,8 +1,11 @@
 ﻿using FinalProjectRestorant.DAL;
+using FinalProjectRestorant.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,5 +26,43 @@ namespace FinalProjectRestorant.Areas.AdminPanel.Controllers
         {
             return View(_context.Menus);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Menu menu)
+        {
+            if (ModelState["Photo"].ValidationState == ModelValidationState.Invalid)
+            {
+                return View();
+            }
+            if (!menu.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Please enter image format");
+                return View();
+            }
+            if (menu.Photo.Length / 1024 > 500)
+            {
+                ModelState.AddModelError("Photo", "image size must be less 500kb");
+                return View();
+            }
+            string fileName = Guid.NewGuid().ToString() + menu.Photo.FileName;
+            string resulPath = Path.Combine(_env.WebRootPath, "photos", "Menu-photo", fileName);
+            using (FileStream fileStream = new FileStream(resulPath, FileMode.Create))
+            {
+                await menu.Photo.CopyToAsync(fileStream);
+            }
+            menu.Image = fileName;
+            _context.Menus.Add(menu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
     }
 }
