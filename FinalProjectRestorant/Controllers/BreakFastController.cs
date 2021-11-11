c8f3660f4dc1e9df5b3874b6d4c4399310ebc9ef
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-11 22:26:21 +0400

    Breakfastiscomplatedclientsidenow

---
+++ FinalProjectRestorant/Controllers/BreakFastController.cs
@@ -0,0 +1,29 @@
+﻿using FinalProjectRestorant.DAL;
+using FinalProjectRestorant.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectRestorant.Controllers
+{
+    public class BreakFastController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BreakFastController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            BreakFastVM breakFastVM = new BreakFastVM
+            {
+                slides = _context.Slides.ToList(),
+                breakFasts = await _context.breakFasts.ToListAsync()
+            };
+            return View(breakFastVM);
+        }
+    }
+}
