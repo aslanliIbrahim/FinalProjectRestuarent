36bb0e7c980659a42e7bf9027225378d2821c609
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-12 02:03:23 +0400

    breakfastmenuiscomplated,fornow

---
+++ FinalProjectRestorant/Areas/AdminPanel/Controllers/BreakFastController.cs
@@ -0,0 +1,73 @@
+﻿using FinalProjectRestorant.DAL;
+using FinalProjectRestorant.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectRestorant.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class BreakFastController : Controller
+    {
+        public AppDbContext _context { get; }
+        public IWebHostEnvironment _env { get; }
+        public BreakFastController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.breakFasts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BreakFast breakFast)
+        {
+            if (ModelState["Photo"].ValidationState == ModelValidationState.Invalid)
+            {
+                return View();
+            }
+            if (!breakFast.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Please enter image format");
+                return View();
+            }
+            if (breakFast.Photo.Length / 1024 > 500)
+            {
+                ModelState.AddModelError("Photo", "image size must be less 500kb");
+                return View();
+            }
+            string fileName = Guid.NewGuid().ToString() + breakFast.Photo.FileName;
+            string resulPath = Path.Combine(_env.WebRootPath, "photos", "Menu-photo", fileName);
+            using (FileStream fileStream = new FileStream(resulPath, FileMode.Create))
+            {
+                await breakFast.Photo.CopyToAsync(fileStream);
+            }
+            breakFast.Image = fileName;
+            _context.breakFasts.Add(breakFast);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //delete is start
+        public IActionResult Delete(int? id)
+        {
+            var menus = _context.breakFasts.FirstOrDefault(bt => bt.Id == id);
+            _context.breakFasts.Remove(menus);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        //delete is end
+    }
+}
