aeecc1a9282618977e9f1d66a3a668c9cd70b03d
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-12 00:37:19 +0400

    steakmenuisready

--- FinalProjectRestorant/Areas/AdminPanel/Controllers/SteakBigMenuController.cs
+++ FinalProjectRestorant/Areas/AdminPanel/Controllers/SteakBigMenuController.cs
@@ -3,6 +3,7 @@ using FinalProjectRestorant.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -59,7 +60,59 @@ namespace FinalProjectRestorant.Areas.AdminPanel.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        //update start here..
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var steak = await _context.SteakBigMenus.FirstOrDefaultAsync(st => st.Id == id);
+            if (steak == null)
+                return NotFound();
+            return View(steak);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, SteakBigMenu steakBigMenu)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            if (id == null)
+                return NotFound();
+
+            if (steakBigMenu == null)
+                return NotFound();
+
+            //remove old img
+            string enviroment = _env.WebRootPath;
+            //string folderpath = Path.Combine(enviroment, "photos", "Our-chef-photo", slides.Photo.FileName) ;
+            //FileInfo oldfile = new FileInfo(folderpath);
+            //if (System.IO.File.Exists(folderpath))
+            //{
+            //    oldfile.Delete();
+            //};
+            //remove end
+
+            //new img in local folder
+            string filename = Guid.NewGuid().ToString() + '-' + steakBigMenu.Photo.FileName;
+            string newSlider = Path.Combine(enviroment, "photos", "Menu-photo", filename);
+            using (FileStream newFile = new FileStream(newSlider, FileMode.Create))
+            {
+                steakBigMenu.Photo.CopyTo(newFile);
+            }
+
+            //new img end 
+            var steakmenuDb = await _context.SteakBigMenus.FirstOrDefaultAsync(st => st.Id == id);
+            steakmenuDb.Image = filename;
+            steakmenuDb.BigMenuFoodName = steakBigMenu.BigMenuFoodName;
+            steakmenuDb.Ingredient = steakBigMenu.Ingredient;
+            steakmenuDb.Pieces = steakBigMenu.Pieces;
+            steakmenuDb.Price = steakBigMenu.Price;
+            steakmenuDb.Servis = steakBigMenu.Servis;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        //update end here..
         //delete is start here..
         public IActionResult Delete(int? id)
         {
