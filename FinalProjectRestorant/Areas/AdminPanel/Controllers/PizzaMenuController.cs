43e20774441c99125aacb13aef3100c56586dc65
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-12 11:55:16 +0400

    Create and delete is ready for pizza menus in adminpanel

--- FinalProjectRestorant/Areas/AdminPanel/Controllers/PizzaMenuController.cs
+++ FinalProjectRestorant/Areas/AdminPanel/Controllers/PizzaMenuController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProjectRestorant.DAL;
+using FinalProjectRestorant.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +14,60 @@ namespace FinalProjectRestorant.Areas.AdminPanel.Controllers
     [Area("AdminPanel")]
     public class PizzaMenuController : Controller
     {
+        public AppDbContext _context { get; }
+        public IWebHostEnvironment _env { get; }
+        public PizzaMenuController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
         public IActionResult Index()
+        {
+            return View(_context.Pizzas);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Pizza pizza)
+        {
+            if (ModelState["Photo"].ValidationState == ModelValidationState.Invalid)
+            {
+                return View();
+            }
+            if (!pizza.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Please enter image format");
+                return View();
+            }
+            if (pizza.Photo.Length / 1024 > 500)
+            {
+                ModelState.AddModelError("Photo", "image size must be less 500kb");
+                return View();
+            }
+            string fileName = Guid.NewGuid().ToString() + pizza.Photo.FileName;
+            string resulPath = Path.Combine(_env.WebRootPath, "photos", "Menu-photo", fileName);
+            using (FileStream fileStream = new FileStream(resulPath, FileMode.Create))
+            {
+                await pizza.Photo.CopyToAsync(fileStream);
+            }
+            pizza.Image = fileName;
+            _context.Pizzas.Add(pizza);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //delete is start
+        public IActionResult Delete(int? id)
+        {
+            var menus = _context.Pizzas.FirstOrDefault(pi => pi.Id == id);
+            _context.Pizzas.Remove(menus);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        //delet end
     }
 }
