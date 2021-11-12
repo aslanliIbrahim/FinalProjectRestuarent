484c12d0071308815836b7239872d49ebb925820
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-12 16:26:12 +0400

    update is ready for desserts adminpanel

--- FinalProjectRestorant/Areas/AdminPanel/Controllers/DessertsController.cs
+++ FinalProjectRestorant/Areas/AdminPanel/Controllers/DessertsController.cs
@@ -3,6 +3,7 @@ using FinalProjectRestorant.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -60,6 +61,59 @@ namespace FinalProjectRestorant.Areas.AdminPanel.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //update is start
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var desserts = await _context.desserts.FirstOrDefaultAsync(bt => bt.Id == id);
+            if (desserts == null)
+                return NotFound();
+            return View(desserts);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Desserts desserts)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            if (id == null)
+                return NotFound();
+
+            if (desserts == null)
+                return NotFound();
+
+            //remove old img
+            string enviroment = _env.WebRootPath;
+            //string folderpath = Path.Combine(enviroment, "photos", "Our-chef-photo", slides.Photo.FileName) ;
+            //FileInfo oldfile = new FileInfo(folderpath);
+            //if (System.IO.File.Exists(folderpath))
+            //{
+            //    oldfile.Delete();
+            //};
+            //remove end
+
+            //new img in local folder
+            string filename = Guid.NewGuid().ToString() + '-' + desserts.Photo.FileName;
+            string newSlider = Path.Combine(enviroment, "photos", "Menu-photo", filename);
+            using (FileStream newFile = new FileStream(newSlider, FileMode.Create))
+            {
+                desserts.Photo.CopyTo(newFile);
+            }
+
+            //new img end   
+            var dessertsDB = await _context.desserts.FirstOrDefaultAsync(st => st.Id == id);
+            dessertsDB.Image = filename;
+            dessertsDB.BigMenuFoodName = desserts.BigMenuFoodName;
+            dessertsDB.Ingredient = desserts.Ingredient;
+            dessertsDB.Pieces = desserts.Pieces;
+            dessertsDB.Price = desserts.Price;
+            dessertsDB.Servis = desserts.Servis;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        //update is end
         //delete is start
         public IActionResult Delete(int? id)
         {
