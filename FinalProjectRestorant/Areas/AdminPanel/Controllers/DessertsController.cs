ec3d09d552541de027a92120df3fa677e8f53204
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-12 16:17:08 +0400

    create ready for desserts in admin panel

--- FinalProjectRestorant/Areas/AdminPanel/Controllers/DessertsController.cs
+++ FinalProjectRestorant/Areas/AdminPanel/Controllers/DessertsController.cs
@@ -1,8 +1,11 @@
 ﻿using FinalProjectRestorant.DAL;
+using FinalProjectRestorant.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +24,40 @@ namespace FinalProjectRestorant.Areas.AdminPanel.Controllers
         public IActionResult Index()
         {
             return View(_context.desserts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Desserts desserts)
+        {
+            if (ModelState["Photo"].ValidationState == ModelValidationState.Invalid)
+            {
+                return View();
+            }
+            if (!desserts.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Please enter image format");
+                return View();
+            }
+            if (desserts.Photo.Length / 1024 > 500)
+            {
+                ModelState.AddModelError("Photo", "image size must be less 500kb");
+                return View();
+            }
+            string fileName = Guid.NewGuid().ToString() + desserts.Photo.FileName;
+            string resulPath = Path.Combine(_env.WebRootPath, "photos", "Menu-photo", fileName);
+            using (FileStream fileStream = new FileStream(resulPath, FileMode.Create))
+            {
+                await desserts.Photo.CopyToAsync(fileStream);
+            }
+            desserts.Image = fileName;
+            _context.desserts.Add(desserts);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
