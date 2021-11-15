1f8f640ab6311b437be9f3cd7a06f6f64341a1d2
aslanliIbrahim <ibrahimra@code.edu.az>
2021-11-15 15:33:19 +0400

    Pagination  ready

--- FinalProjectRestorant/Controllers/SteakBigMenuController.cs
+++ FinalProjectRestorant/Controllers/SteakBigMenuController.cs
@@ -1,4 +1,5 @@
 ﻿using FinalProjectRestorant.DAL;
+using FinalProjectRestorant.Models;
 using FinalProjectRestorant.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,12 +17,24 @@ namespace FinalProjectRestorant.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
+            decimal pageItemCount = 3;
+            decimal b = Math.Ceiling(_context.SteakBigMenus.Count() / pageItemCount);
+            ViewBag.pageCount =Convert.ToInt32(b);
+            ViewBag.ActivePage = page;
+
+
+            List<SteakBigMenu> steakBigMenus = await _context.SteakBigMenus.OrderBy(st=>st.Id)
+                                                                           .Skip((page - 1) * (int)pageItemCount)
+                                                                           .Take((int)pageItemCount)
+                                                                           .ToListAsync();
+
+
             SteakBigMenuVM steakBigMenu = new SteakBigMenuVM
             {
                 slides = _context.Slides.ToList(),
-                steakBigMenus = await _context.SteakBigMenus.ToListAsync()
+                steakBigMenus = steakBigMenus
             };
             return View(steakBigMenu);
         }
